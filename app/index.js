92b5066b0703f88538f0db57b4de8e393a4a16f8
Fhillip G. Castillo <fcastillo@intellisys.com.do>
2017-06-06 17:31:09 -0400

    add new basic hello world and configs

--- app/index.js
+++ app/index.js
@@ -3,6 +3,24 @@ var ReactDom = require('react-dom');
 require("./index.css")
 /*React.createClass()*/
 
+// state
+// lifecycle event
+// UI *required this allway need to be set
+
+
 class App extends React.Component {
-	
+	render(){
+		return (
+			<div>
+				Hello World!
+			</div>
+		)
+	}
 }
+
+// <App />
+
+ReactDom.render(
+	<App />,
+	document.getElementById('app')
+);
