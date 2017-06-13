d93b7f02e0bdabaec6c36ff4268819b9cd908035
Fhillip G. Castillo <gerson.fhillip@gmail.com>
2017-06-13 14:24:38 -0400

    design routes

--- app/components/App.js
+++ app/components/App.js
@@ -1,12 +1,31 @@
 var React = require('react'),
-	Popular = require('./Popular');
+	Popular = require('./Popular'),
+	ReactRouter = require('react-router-dom'),
+	Nav = require('./Nav'),
+	Battle = require('./Battle'),
+	Home = require('./Home');
+
+var Router = ReactRouter.BrowserRouter,
+	Switch = ReactRouter.Switch,
+	Route = ReactRouter.Route;
 
 class App extends React.Component {
 	render(){
 		return (
-			<div className="container">
-				<Popular />
-			</div>
+			<Router>
+				<div className="container">
+					<Nav />
+					<Switch>{/*render only an expecific route*/}
+						<Route exact path="/" component={Home} />
+						<Route path="/battle" component={Battle} />
+						<Route path="/popular" component={Popular} />
+						<Route path="/popular" component={Popular} />
+						<Route render={function(){
+							return <p>Not Found</p>;
+						}}/>
+					</Switch>
+				</div>
+			</Router>
 		)
 	}
 };
---
+++ app/components/Nav.js
@@ -0,0 +1,27 @@
+var React = require('react'),
+	Link = require('react-router-dom').Link,/*only to display a url or link*/
+	NavLink = require('react-router-dom').NavLink; //to change some state depending if your at that dir
+
+function Nav(){
+	return (
+		<ul className="nav">
+			<li>
+				<NavLink exact activeClassName="active" to="/">
+					Home
+				</NavLink>
+			</li>
+			<li>
+				<NavLink activeClassName="active" to="/battle">
+					Battle
+				</NavLink>
+			</li>
+			<li>
+				<NavLink activeClassName="active" to="/popular">
+					Popular
+				</NavLink>
+			</li>
+		</ul>
+	)
+};
+
+module.exports = Nav;
