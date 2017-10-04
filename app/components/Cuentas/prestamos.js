8ba25b699bf37a6d2a3603eb12ecc9519291ce00
Ing. Fhillip Castillo <gerson.fhillip@gmail.com>
2017-10-03 23:14:41 -0400

    move cuentas to components

---
+++ app/components/Cuentas/prestamos.js
@@ -0,0 +1,19 @@
+const React = require('react'),
+	const PropTypes = require('prop-types');
+
+class Prestamo extends React.Component{
+	render(){
+		return (<h1>Hello WOrld Prestamos</h1>>)
+	}
+} ;
+
+// Prestamos.propTypes = {
+// 	id : 0,
+// 	IBO:11111,/*opcional*/
+// 	Nombre:"Fulano",
+// 	Producto: 'Glister',
+// 	ProductoId: 123412,
+// 	Fecha: "hoy",
+// 	PrecioAcordado: false,
+// 	Precio: 0/*opcional*/
+// }
