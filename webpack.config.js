96c779eb6e971f1b6cdbf4c44a9ce9dd6079c1ad
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-05-20 19:54:30 -0400

    change webpack optimizer

--- webpack.config.js
+++ webpack.config.js
@@ -27,18 +27,25 @@ const config = {
       template: "app/index.html",
     }),
   ],
+  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
 };
 
 if (process.env.NODE_ENV === "production") {
-  config.plugins.push(
-    new webpack.DefinePlugin({
-      "process.env": {
-        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
-      },
-    }),
-    new webpack.optimize.UglifyJsPlugin()
-  );
-} else {
-}
+  config.optimization = {
+    minimizer: [
+      // we specify a custom UglifyJsPlugin here to get source maps in production
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  }
+} 
 
 module.exports = config;
