0ed14ba9fb7453e483d4c2625148571ce2a0d5b2
Fhillip Castillo <gerson.fhillip@gmail.com>
2020-05-19 22:37:44 -0400

    upgrade project to more recent versions

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,44 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var webpack = require('webpack');
-
+var path = require("path");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var webpack = require("webpack");
 
 //NODE_ENV to production
-//uglify  
-
+//uglify
 
 const config = {
-  entry: './app/index.js',
+  entry: "./app/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'index_bundle.js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "index_bundle.js",
   },
   module: {
     rules: [
-      { test: /\.(js)$/, use: 'babel-loader'},
-      { test: /\.css$/, use: [ 'style-loader', 'css-loader' ]}
+      { test: /\.(js)$/, use: "babel-loader" },
+      { test: /\.css$/, use: ["style-loader", "css-loader"] },
+      { test: /\.(jsx)$/, use: "babel-loader" },
     ],
-    loaders: [
-      { test: /\.jsx?/, exclude: /node_modules/ , loader:'babel-loader'}
-    ]
   },
   /*to fix the reflesh issue*/
   devServer: {
-    historyApiFallback: true
+    historyApiFallback: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: 'app/index.html'
-    })
-  ]
+      template: "app/index.html",
+    }),
+  ],
 };
 
 if (process.env.NODE_ENV === "production") {
   config.plugins.push(
     new webpack.DefinePlugin({
-      'process.env' : {
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-      }
+      "process.env": {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+      },
     }),
     new webpack.optimize.UglifyJsPlugin()
-  )
-} else {}
+  );
+} else {
+}
 
 module.exports = config;
